17c262287f5cc22eb4a1e10353a988ff5acd2a91
zhifu <zhifu@Ocsens-MacBook-Pro.local>
2016-11-27 15:54:37 +0800

    添加 knockout-validation 支持，并初始化

--- src/app/startup.ts
+++ src/app/startup.ts
@@ -1,4 +1,6 @@
 /// <amd-dependency path="bootstrap" />
+/// <amd-dependency path="knockout-postbox" />
+/// <amd-dependency path="knockout-validation" />
 
 import $ = require("jquery");
 import ko = require("knockout");
@@ -9,6 +11,42 @@ ko.components.register('home-page', { require: 'components/home-page/home' });
 ko.components.register('about-page', {
     template: { require: 'text!components/about-page/about.html' }
 });
+ko.validation.init({
+    errorElementClass: 'has-error',
+    errorMessageClass: 'help-block',
+    decorateElement: true,
+    insertMessages: false
+});
+ko.validation.rules['EncodeMaxLength'] = {
+    validator: (value, otherVal) => {
+        if (_.isUndefined(value) || _.isNull(value)) {
+            return true;
+        } else {
+            return value.length + (encodeURIComponent(value).match(/%[89ABab]/g) ? encodeURIComponent(value).match(/%[89ABab]/g).length/2 : 0) <= otherVal;
+        }
+    },
+    message: '长度不能超过{0}'
+};
+ko.validation.registerExtenders();
+ko.validation.localize({
+    required: '必填字段',
+    min: '必须大于等于 {0}',
+    max: '必须小于等于 {0}',
+    minLength: '至少输入 {0} 个字符',
+    maxLength: '字符数不能超过 {0} 个',
+    pattern: '请检查此值',
+    step: '每次步进值是 {0}',
+    email: 'email格式不正确',
+    date: '日期格式不正确',
+    dateISO: '日期格式不正确',
+    number: '请输入数字',
+    digit: '请输入数字',
+    phoneUS: '手机号(US)不合法',
+    equal: '输入值不一样',
+    notEqual: '请选择另一个值',
+    unique: '此值应该是唯一的'
+});
+
 class App {
     public route = router.currentRoute;
 }
