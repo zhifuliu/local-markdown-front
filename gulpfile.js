14890bcbc4c6d5974facfce017ce0dc12c65eb1b
zhifu <zhifu@Ocsens-MacBook-Pro.local>
2016-11-27 10:33:16 +0800

    task 整理

--- gulpfile.js
+++ gulpfile.js
@@ -16,17 +16,11 @@ var gulp = require('gulp'),
     typescript = require('gulp-tsc'),
     webServer = require('gulp-webserver'),
     gutil = require('gulp-util'),
-    sass = require('gulp-sass');
-
-gulp.task('test', ['ts'], function() {
-    new KarmaServer({
-        configFile: __dirname + '/karma.conf.js',
-        action: 'run'
-    }).start();
-});
+    sass = require('gulp-sass'),
+    gulpsync = require('gulp-sync')(gulp);
 
 // WebServer
-gulp.task('webserver', ['auto-ts'], function() {
+gulp.task('webserver', ['auto-ts', 'auto-sass'], function() {
     gulp.src('./src')
         .pipe(webServer({
             host: '0.0.0.0',
@@ -38,20 +32,6 @@ gulp.task('webserver', ['auto-ts'], function() {
             directoryListing: true,
             port: 1314
         }));
-});
-
-gulp.task('auto-ts', ['ts'], function() {
-    gulp.watch('**/*.ts', function() {
-        gulp.src(['**/*.ts'])
-            .pipe(typescript({
-                module: 'amd',
-                sourcemap: false,
-                outDir: './',
-                target: 'es5'
-            }))
-            .on('error', gutil.log)
-            .pipe(gulp.dest('./'));
-    });
 });
 
 var requireJsRuntimeConfig = vm.runInNewContext(fs.readFileSync('src/app/require.config.js') + '; require;');
@@ -69,10 +49,8 @@ requireJsOptimizerConfig = merge(requireJsRuntimeConfig, {
         'text!components/about-page/about.html'
     ],
     insertRequire: ['app/startup'],
-    bundles: {
-    }
+    bundles: {}
 });
-
 
 // Compile all .ts files, producing .js and source map files alongside them
 gulp.task('ts', function() {
@@ -85,6 +63,19 @@ gulp.task('ts', function() {
         }))
         .pipe(gulp.dest('./'));
 });
+gulp.task('auto-ts', ['ts'], function() {
+    gulp.watch('**/*.ts', function() {
+        gulp.src(['**/*.ts'])
+            .pipe(typescript({
+                module: 'amd',
+                sourcemap: false,
+                outDir: './',
+                target: 'es5'
+            }))
+            .on('error', gutil.log)
+            .pipe(gulp.dest('./'));
+    });
+});
 
 // Discovers all AMD dependencies, concatenates together all required .js files, minifies them
 gulp.task('js', ['ts'], function() {
@@ -95,18 +86,6 @@ gulp.task('js', ['ts'], function() {
         .pipe(gulp.dest('./dist/'));
 });
 
-// Concatenates CSS files, rewrites relative paths to Bootstrap fonts, copies Bootstrap fonts
-gulp.task('css', function() {
-    var bowerCss = gulp.src('src/bower_modules/components-bootstrap/css/bootstrap.min.css')
-        .pipe(replace(/url\((')?\.\.\/fonts\//g, 'url($1fonts/')),
-        appCss = gulp.src('src/css/*.css'),
-        combinedCss = es.concat(bowerCss, appCss).pipe(concat('css.css')),
-        fontFiles = gulp.src('./src/bower_modules/components-bootstrap/fonts/!*', {
-            base: './src/bower_modules/components-bootstrap/'
-        });
-    return es.concat(combinedCss, fontFiles)
-        .pipe(gulp.dest('./dist/'));
-});
 gulp.task('auto-sass', ['sass'], function() {
     gulp.watch('./src/css/*.scss', ['sass']);
 });
@@ -118,6 +97,18 @@ gulp.task('sass', function() {
             }).on('error', sass.logError)
         )
         .pipe(gulp.dest('./src/css'));
+});
+// Concatenates CSS files, rewrites relative paths to Bootstrap fonts, copies Bootstrap fonts
+gulp.task('css', ['sass'] function() {
+    var bowerCss = gulp.src('src/bower_modules/components-bootstrap/css/bootstrap.min.css')
+        .pipe(replace(/url\((')?\.\.\/fonts\//g, 'url($1fonts/')),
+        appCss = gulp.src('src/css/*.css'),
+        combinedCss = es.concat(bowerCss, appCss).pipe(concat('css.css')),
+        fontFiles = gulp.src('./src/bower_modules/components-bootstrap/fonts/!*', {
+            base: './src/bower_modules/components-bootstrap/'
+        });
+    return es.concat(combinedCss, fontFiles)
+        .pipe(gulp.dest('./dist/'));
 });
 // Copies index.html, replacing <script> and <link> tags to reference production URLs
 gulp.task('html', function() {
@@ -134,17 +125,16 @@ gulp.task('clean', function() {
     var distContents = gulp.src('./dist/**/*', {
             read: false
         }),
-        generatedJs = gulp.src(['src/**/*.js', 'src/**/*.js.map', 'test/**/*.js', 'test/**/*.js.map'], {
+        generatedJs = gulp.src(['src/**/*.js', 'src/**/*.js.map', 'test/**/*.js', 'test/**/*.js.map', '**/.DS_Store'], {
             read: false
         })
         .pipe(es.mapSync(function(data) {
-            // Include only the .js/.js.map files that correspond to a .ts file
             return fs.existsSync(data.path.replace(/\.js(\.map)?$/, '.ts')) ? data : undefined;
         }));
     return es.merge(distContents, generatedJs).pipe(clean());
 });
 
-gulp.task('default', ['html', 'js', 'css'], function(callback) {
+gulp.task('default', gulpsync.sync['html', 'js', 'css'], function(callback) {
     callback();
     console.log('\nPlaced optimized files in ' + chalk.magenta('dist/\n'));
 });
